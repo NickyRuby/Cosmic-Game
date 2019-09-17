34e3cecfe6ef37e58450204029aebbb21ac212c5
Mykyta Lobyntsev <you@gmail.com>
2019-09-17 21:08:12 +0300

    Trying to spin Sputnik

--- game.js
+++ game.js
@@ -17,10 +17,12 @@ var SUN_ORBIT_RAIDUS_1 = 60;
 var SUN_ORBIT_RAIDUS_2 = 100;
 var SUN_ORBIT_RAIDUS_3 = 150;
 var SUN_ORBIT_COLOR = "#c1c1c1";
+
+// MOVING OBJECTS — x,y:center of orbit; raduis: radius of orbit; rad: planet's radius;
 var EARTH_1 = {x: SUN_X_POS, y: SUN_X_POS, radius: SUN_ORBIT_RAIDUS_1, rad: EARTH_RADIUS, color: "blue" }
 var EARTH_2 = {x: SUN_X_POS, y: SUN_X_POS, radius: SUN_ORBIT_RAIDUS_2, rad: EARTH_RADIUS, color: "purple" }
 var EARTH_3 = {x: SUN_X_POS, y: SUN_X_POS, radius: SUN_ORBIT_RAIDUS_3, rad: EARTH_RADIUS, color: "black" }
-var MOON = {x: EARTH_1.x + SUN_ORBIT_RAIDUS_1, y:EARTH_1.y + SUN_ORBIT_RAIDUS_1, radius: EARTH_RADIUS * 5 , rad: EARTH_RADIUS / 3, color: "black" }
+var MOON = {x: EARTH_1.x + SUN_ORBIT_RAIDUS_1, y:EARTH_1.y + SUN_ORBIT_RAIDUS_1, radius: EARTH_RADIUS * 3 , rad: EARTH_RADIUS / 2, color: "black" }
 
 
 // Preset World
@@ -64,17 +66,54 @@ function draw(ws) {
   drawMovingBody(EARTH_1,ws);
   drawMovingBody(EARTH_2,ws/2);
   drawMovingBody(EARTH_3,ws/3);
-  drawMovingBody(MOON,ws);
+  drawSputnik(MOON,ws * 4);
+}
+
+// doesn't work properly
+function updateXY(obj,ws) { 
+  obj.x =  obj.x //100 + obj.radius * Math.cos(ws * Math.PI / 180); // + Math.cos(ws * Math.PI / 180);
+  obj.y = obj.y // 100 + obj.radius * Math.sin(ws * Math.PI / 180); // + Math.sin(ws * Math.PI / 180);
+}
+
+function drawSputnik (sputnik,ws) {
+  updateXY(sputnik,ws);
+  drawMovingBody(sputnik,ws);
 }
 
 
 // make planets spin
 function drawMovingBody(obj,ws) { 
- drawCircle(obj.x - obj.radius * Math.cos(ws * Math.PI / 180), obj.y + obj.radius * Math.sin (ws * Math.PI / 180), obj.rad, obj.color, true)
+ drawCircle(obj.x - obj.radius * Math.cos(ws * Math.PI / 180), 
+ obj.y + obj.radius * Math.sin (ws * Math.PI / 180), obj.rad, obj.color, true)
 }
 
 
 
+// WorldState -> WorldState
+// Change Earth position
+function tick(ws) {
+  return ws + 1;
+}
+
+function bigBang(ws, onDraw, onTick) {
+  requestAnimationFrame(function(time) {
+    onDraw(ws);
+    var newWS = onTick(ws);
+    bigBang(newWS, onDraw, onTick)
+  });
+}
+
+function forTest(){
+  console.log("hello.world");
+}
+
+bigBang(0, draw, tick);
+
+
+// Сделать класс Body 
+// создаем каждый объект 
+// по тику мы апдейтим объект 
+// для вращения спутника центром орбиты берем координаты центра Земли
 
 /*
 
@@ -92,30 +131,4 @@ function drawMovingBody(obj,ws) {
     }
   }
 
-
-  let earth = new Body(1,2,3,0);
-  // update x,y value -> draw updated bodies
-  earth.update(ws)
-
-
-  // for Sputnik
-  drawcircle(earth.x + moon.x)
-
 */
-
-
-// WorldState -> WorldState
-// Change Earth position
-function tick(ws) {
-  return ws + 1 ;
-}
-
-function bigBang(ws, onDraw, onTick) {
-  requestAnimationFrame(function(time) {
-    onDraw(ws);
-    var newWS = onTick(ws);
-    bigBang(newWS, onDraw, onTick)
-  });
-}
-
-bigBang(0, draw, tick);
